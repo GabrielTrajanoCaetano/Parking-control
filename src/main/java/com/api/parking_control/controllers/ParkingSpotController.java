ffb16ee3cab38a205cdd4c986f1aeaeec6e7acd1
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-08-11 22:24:52 -0300

    Trabalhando nos endPoints

---
+++ src/main/java/com/api/parking_control/controllers/ParkingSpotController.java
@@ -0,0 +1,36 @@
+package com.api.parking_control.controllers;
+
+import com.api.parking_control.dtos.ParkingSpotDto;
+import com.api.parking_control.models.ParkingSpotModel;
+import com.api.parking_control.repositories.ParkingSpotRepository;
+import com.api.parking_control.services.ParkingSpotService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+
+    @Autowired
+    public ParkingSpotService parkingSpotService;
+
+    @Autowired
+    public ParkingSpotRepository parkingSpotRepository;
+
+
+    @PostMapping("/post")
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+        var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
+}
---
+++ src/main/java/com/api/parking_control/services/ParkingSpotService.java
@@ -0,0 +1,20 @@
+package com.api.parking_control.services;
+
+import com.api.parking_control.models.ParkingSpotModel;
+import com.api.parking_control.repositories.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    @Autowired
+    ParkingSpotRepository repository;
+
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return repository.save(parkingSpotModel);
+    }
+}
